f351de3cbb8f7690edca5624d7ce5a6166753fe2
Petr Taranenko <petr.tarantul@gmail.con>
2023-02-07 00:03:34 +0700

    link ui, navbar widget

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,19 @@
-import { Suspense } from "react";
-import { Route, Routes, Link } from "react-router-dom";
 import { classNames } from "shared/lib/classNames/classNames";
 
 import "./styles/index.scss";
 
 import { UseTheme } from "app/providers/ThemeProviders/lib/useTheme";
-
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import { AppRouter } from "app/providers/router";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
   const { theme, toggleTheme } = UseTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
+      <Navbar />
+      <AppRouter />
       <button onClick={toggleTheme}>Toggle</button>
-      <Link to={"/"}>Главная</Link>
-      <Link to={"/about"}>О нас</Link>
-      <Suspense fallback={<div>Загрузка...</div>}>
-        <Routes>
-          <Route path={"/about"} element={<AboutPage />} />
-          <Route path={"/"} element={<MainPage />} />
-        </Routes>
-      </Suspense>
     </div>
   );
 };
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import { classNames } from "shared/lib/classNames/classNames";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+
+import cls from "./Navbar.module.scss";
+
+interface NavbarProps {
+  className?: string;
+}
+
+export const Navbar: FC<NavbarProps> = ({ className }) => {
+  return (
+    <div className={classNames(cls.Navbar, {}, [className])}>
+      <div className={cls.links}>
+        <AppLink theme={AppLinkTheme.SECONDARY} to={"/"} className={cls.mainLink}>
+          Главная
+        </AppLink>
+        <AppLink theme={AppLinkTheme.SECONDARY} to={"/about"}>
+          О нас
+        </AppLink>
+      </div>
+    </div>
+  );
+};
