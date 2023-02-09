98ce9490662c1a092a4ab364ff2f06ced824d227
Petr Taranenko <petr.tarantul@gmail.con>
2023-02-09 22:30:23 +0700

    hot loader. babel loader for i18n

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -17,6 +17,18 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     ],
   };
 
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+        plugins: [["i18next-extract", { locales: ["ru", "en"], keyAsDefaultValue: true }]],
+      },
+    },
+  };
+
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -40,5 +52,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     exclude: /node_modules/,
   };
 
-  return [fileLoader, svgLoader, typescriptLoader, cssLoader];
+  return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
 }
