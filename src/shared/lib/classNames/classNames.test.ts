7cc3b170497d40accd220c3c7901212f4a1ec240
Petr Taranenko <petr.tarantul@gmail.con>
2023-02-12 13:45:22 +0700

    jest, test classNames

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,23 @@
+import { classNames } from "./classNames";
+
+describe("classNames", () => {
+  test("with first param", () => {
+    expect(classNames("soneClass")).toBe("soneClass");
+  });
+  test("with additional class", () => {
+    const expected = "soneClass class1 class2";
+    expect(classNames("soneClass", {}, ["class1", "class2"])).toBe(expected);
+  });
+  test("with mods", () => {
+    const expected = "soneClass class1 class2 hovered scrollable";
+    expect(classNames("soneClass", { hovered: true, scrollable: true }, ["class1", "class2"])).toBe(expected);
+  });
+  test("with mods false", () => {
+    const expected = "soneClass class1 class2 hovered";
+    expect(classNames("soneClass", { hovered: true, scrollable: false }, ["class1", "class2"])).toBe(expected);
+  });
+  test("with mods undefined", () => {
+    const expected = "soneClass class1 class2 hovered";
+    expect(classNames("soneClass", { hovered: true, scrollable: undefined }, ["class1", "class2"])).toBe(expected);
+  });
+});
