0e82ee3048eb29232a7b3c0b97bc491ed1757e0c
Petr Taranenko <petr.tarantul@gmail.con>
2023-02-23 14:11:15 +0700

    husky, ui test

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -9,6 +9,5 @@ describe("Button", () => {
   test("clear  theme", () => {
     render(<Button theme={ButtonTheme.CLEAR}>TEST</Button>);
     expect(screen.getByText("TEST")).toHaveClass("clear");
-    screen.debug();
   });
 });
