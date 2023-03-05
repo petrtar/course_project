08000cee6b9dd7b28fa10acfc2ba057dd2e79edc
Petr Taranenko <petr.tarantul@gmail.con>
2023-03-05 12:05:07 +0700

    webpack config for storybook

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -14,20 +14,20 @@ export default ({ config }: { config: webpack.Configuration }) => {
     src: path.resolve(__dirname, "..", "..", "src"),
   };
 
-  config.resolve?.modules?.push(paths.src);
-  config.resolve?.extensions?.push(".ts", ".tsx");
+  config.resolve!.modules!.push(paths.src);
+  config.resolve!.extensions!.push(".ts", ".tsx");
 
-  //@ts-ignore
-  config.module?.rules = config.module?.rules?.map((rule: RuleSetRule) => {
+  const rules = config.module!.rules as RuleSetRule[];
+  config.module!.rules = rules.map((rule) => {
     if (/svg/.test(rule.test as string)) {
       return { ...rule, exclude: /\.svg$/i };
     }
     return rule;
   });
 
-  config.module?.rules?.push(buildSvgLoader());
-  config.module?.rules?.push(buildCssLoader(true));
-  config.plugins?.push(new DefinePlugin({ __IS_DEV__: true, __API__: "" }));
+  config.module!.rules!.push(buildSvgLoader());
+  config.module!.rules!.push(buildCssLoader(true));
+  config.plugins!.push(new DefinePlugin({ __IS_DEV__: true, __API__: "" }));
 
   return config;
 };
