3387604823b0442adb28031a285509de9ee6a79a
Petr Taranenko <petr.tarantul@gmail.con>
2023-03-07 09:06:14 +0700

    fix portal

--- src/shared/ui/Portal/Portal.tsx
+++ src/shared/ui/Portal/Portal.tsx
@@ -11,7 +11,7 @@ export const Portal: FC<PortalProps> = ({ children, element = document.body }) =
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    if (ref.current) ref.current = document.querySelector("#root") || null;
+    ref.current = document.querySelector("#root") || null;
     setMounted(true);
   }, []);
   return mounted && ref.current ? createPortal(children, ref.current) : null;
