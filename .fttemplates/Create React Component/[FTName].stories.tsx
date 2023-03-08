80ede3de0f04a2d9020031fcacacd495e0a3b65f
Petr Taranenko <petr.tarantul@gmail.con>
2023-03-08 22:09:04 +0700

    templates

---
+++ .fttemplates/Create React Component/[FTName].stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { <FTName> } from "./[FTName]";
+import { ThemeDecorator } from "shared/config/storybook/ThemeDecorator";
+import { Theme } from "app/providers/ThemeProviders";
+
+export default {
+  title: "shared/<FTName>",
+  component: <FTName>,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof <FTName>>;
+
+const Template: ComponentStory<typeof <FTName>> = (args) => <<FTName> {...args} />;
+
+export const Normal = Template.bind({});
+Primary.args = {};
---
+++ .fttemplates/Create React Component/[FTName].tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+
+import cls from './[FTName].module.scss';
+
+interface <FTName>Props {
+    className?: string;
+}
+
+export const <FTName>: FC<<FTName>Props> = ({ className }) => {
+    const {t} = useTranslation();
+    return (
+        <div className={classNames(cls.<FTName>, {}, [className])}>
+        </div>
+    );
+};
