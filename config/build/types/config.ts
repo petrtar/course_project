8ee145045e973e9d92b9dc456e8dd64314a345ad
petr <petr.tarantul@gmail.com>
2023-04-03 21:12:29 +0700

    fix lint, fix storybook

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -3,25 +3,25 @@ export type BuildMode = "production" | "development";
 export type ProjectType = "storybook" | "frontend" | "jest";
 
 export interface BuildPath {
-  entry: string;
-  build: string;
-  html: string;
-  src: string;
-  locales: string;
-  buildLocales: string;
+    entry: string;
+    build: string;
+    html: string;
+    src: string;
+    locales: string;
+    buildLocales: string;
 }
 
 export interface BuildEnv {
-  mode: BuildMode;
-  port: number;
-  apiUrl: string;
+    mode: BuildMode;
+    port: number;
+    apiUrl: string;
 }
 
 export interface BuildOptions {
-  mode: BuildMode;
-  paths: BuildPath;
-  isDev: boolean;
-  port: number;
-  apiUrl: string;
-  project: ProjectType;
+    mode: BuildMode;
+    paths: BuildPath;
+    isDev: boolean;
+    port: number;
+    apiUrl: string;
+    project: ProjectType;
 }
--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -13,6 +13,8 @@ export default ({ config }: { config: webpack.Configuration }) => {
         html: "",
         entry: "",
         src: path.resolve(__dirname, "..", "..", "src"),
+        locales: "",
+        buildLocales: "",
     };
 
     config.resolve!.modules!.push(paths.src);
