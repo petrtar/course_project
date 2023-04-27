8b036236ae5a00828a8889212b22d31e13753fa2
Petr <petr@cupcakedev.com>
2023-04-27 17:16:33 +0700

    webpack config upgrade

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -5,7 +5,9 @@ import { buildPlugins } from "./buildPlugins";
 import { buildResolvers } from "./buildResolvers";
 import { BuildOptions } from "./types/config";
 
-export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+export function buildWebpackConfig(
+    options: BuildOptions
+): webpack.Configuration {
     const { paths, mode, isDev } = options;
 
     return {
@@ -22,7 +24,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
             rules: buildLoaders(options),
         },
         resolve: buildResolvers(options),
-        devtool: isDev ? "inline-source-map" : undefined,
+        devtool: isDev ? "eval-cheap-module-source-map" : undefined,
         devServer: isDev ? buildDevServer(options) : undefined,
     };
 }
